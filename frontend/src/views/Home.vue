0f75d6614c2b58c1f07f5247d183c0fba814ba50
Stig Fredrik Johansson <fredrik.johansson@iths.se>
2021-02-24 14:50:26 +0100

    cleanup after helloworld

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
   </div>
 </template>
 
@@ -10,7 +9,6 @@
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
