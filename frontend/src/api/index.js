418edd7694aef4ac6bf82b93eef48a4b8b63dec0
renzosantamaria <renzo.santamaria@outlook.com>
2021-02-25 11:23:23 +0100

    add cleaner Axios syntax

---
+++ frontend/src/api/index.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api'
+})
+
+// Products
+
+export async function fetchProducts() {
+  const products = await API.get('/products/')
+  return products.data
+}
+
+
+export async function submitOrder(payload, userToken) {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${userToken}`
+    }
+  }
+
+  try {
+    const response = await API.get('/orders', payload, config)
+    return response.data
+  } catch (e) {
+    return false
+  }
+}
+
+// User
+
+export async function registerUser(payload) {
+  const response = await API.get('/register/', payload)
+
+  if (response.status === 200) {
+    return true
+  }
+
+  return false
+}
+
+export async function authenticate(payload) {
+  try {
+    const response = await API.get('/auth/', payload)
+    return response.data
+  } catch (e) {
+    return false
+  }
+}
