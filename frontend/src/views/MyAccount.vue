f9ba38c2b44dbb282f4141f879c131784f5ddfdc
marcuslindqvist <marcus.lindqvist@iths.se>
2021-02-25 14:41:03 +0100

    removed header + footer from views

--- frontend/src/views/MyAccount.vue
+++ frontend/src/views/MyAccount.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-    <Header />
-    <Footer />
+    
     this is my god damn account</div>
 </template>
 
 <script>
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
+
 export default {
-components: {
-  Header,
-  Footer
-},
+
 };
 </script>
 
