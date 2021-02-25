42ebe9237f36463a46cf3062ef0cf269f1fc6bba
Monika-Khadgi <monika.khadgi@iths.se>
2021-02-25 15:05:52 +0100

    product desc
    product

--- frontend/src/components/ProductDescription.vue
+++ frontend/src/components/ProductDescription.vue
@@ -1,4 +1,17 @@
 <template>
+<div class="productDescription">
+    <div class="productPhoto"></div>
+    <div class="description"></div>
+    <div class="size">
+        <p> Size </p>
+        <button id="small">S</button>
+        <button id="medium">M</button>
+        <button id="large">L</button>
+    </div>
+    <div class="review"></div>
+
+    <button>Add to cart</button>
+</div>
   
 </template>
 
---
+++ frontend/src/views/Description.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    products description shown here
+    <ProductDescription />
+  </div>
+</template>
+
+<script>
+import ProductDescription from "@/components/ProductDescription.vue";
+export default {
+  components: {
+    ProductDescription,
+  },
+};
+</script>
+
+<style>
+</style>
