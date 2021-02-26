57aea47f3a79b1f4dc8dac169c422a818aa7b396
PelleLindroth <pellelindroth@gmail.com>
2021-02-26 10:10:13 +0100

    Add actions for getUser and updateUser

--- frontend/src/store/user.store.js
+++ frontend/src/store/user.store.js
@@ -13,7 +13,6 @@ export default {
     },
     [Mutations.SET_LOGGED_IN](state, payload) {
       state.isLoggedIn = payload
-      console.log(state.isLoggedIn)
     },
     [Mutations.SET_USER_TOKEN](state, payload) {
       state.userToken = payload
@@ -27,9 +26,18 @@ export default {
         await dispatch('logIn', { email: payload.email, password: payload.password })
       }
     },
+    async getUser({ commit }, payload) {
+      const user = await API.getUser(payload)
+
+      if (user) {
+        commit(Mutations.SET_USER_TOKEN, payload)
+        commit(Mutations.SET_CURRENT_USER, user)
+        commit(Mutations.SET_LOGGED_IN, true)
+      }
+    },
     async logIn({ commit }, payload) {
       const response = await API.authenticate(payload)
-      console.log(response);
+
       if (response) {
         commit(Mutations.SET_USER_TOKEN, response.token)
         commit(Mutations.SET_CURRENT_USER, response.user)
@@ -40,6 +48,15 @@ export default {
       commit(Mutations.SET_USER_TOKEN, null)
       commit(Mutations.SET_CURRENT_USER, null)
       commit(Mutations.SET_LOGGED_IN, false)
+      API.clearStorage()
+    },
+    async updateUser({ state, commit }, payload) {
+      const response = await API.updateUser(payload, state.userToken)
+
+      if (response.status == 200) {
+        const user = await API.getUser(state.userToken)
+        commit(Mutations.SET_CURRENT_USER, user)
+      }
     }
   },
   getters: {
