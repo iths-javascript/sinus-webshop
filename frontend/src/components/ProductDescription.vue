690c6519540791421ab8128358c8cba841f552fe
marcuslindqvist <marcus.lindqvist@iths.se>
2021-03-03 13:11:14 +0100

    changed width to 100vw

--- frontend/src/components/ProductDescription.vue
+++ frontend/src/components/ProductDescription.vue
@@ -6,19 +6,31 @@
       </div>
       <div class="product-decriptionbox">
         <table>
-          <tr class="product-title">{{ product.title }}</tr>
-          <tr class="product-described">{{ product.longDesc }}</tr>
-          <tr class="product-price">{{ product.price }} sek</tr>
+          <tr class="product-title">
+            {{
+              product.title
+            }}
+          </tr>
+          <tr class="product-described">
+            {{
+              product.longDesc
+            }}
+          </tr>
+          <tr class="product-price">
+            {{
+              product.price
+            }}
+            sek
+          </tr>
           <tr class="product-size"></tr>
           <tr class="product-review"></tr>
 
           <button class="product-add">Take my money</button>
-        </table>  
+        </table>
       </div>
       <span class="product-closeimage" @click="handleClick">X</span>
-    <div>
-  </div>
-  </div>
+      <div></div>
+    </div>
   </div>
 </template>
 
@@ -61,97 +73,91 @@ export default {
 </script>
 
 <style lang="scss">
-
 table {
   border-collapse: collapse;
   width: 100%;
 }
 tr {
-// border: 1px solid #ddd;
+  // border: 1px solid #ddd;
   text-align: left;
 }
 .centered {
-    float: none;
-    margin: 0 auto;
-    text-align: center;
-    vertical-align: middle;
-    align-items: center
-}
-
-.product{
-&-description {
-  display: flex;
-  justify-content: center;
+  float: none;
+  margin: 0 auto;
+  text-align: center;
+  vertical-align: middle;
   align-items: center;
-  position: absolute;
-  width: 1440px;
-  height:900px;
-  left: -8px;
-  top: 0px;
-  background-color:rgba(0, 0, 0, 0.85);
-  
 }
 
-&-innerbox{
-  display: flex;
-  background-color:white;
-  width: 768px;
-  height: 508px;
-}
+.product {
+  &-description {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    width: 100vw;
+    height: 900px;
+    left: -8px;
+    top: 0px;
+    background-color: rgba(0, 0, 0, 0.85);
+  }
 
+  &-innerbox {
+    display: flex;
+    background-color: white;
+    width: 768px;
+    height: 508px;
+  }
 
-&-decriptionbox {
-  margin-top: 2rem;
-}
+  &-decriptionbox {
+    margin-top: 2rem;
+  }
 
-&-described {
-font-family: Open Sans;
-font-style: normal;
-font-weight: normal;
-font-size: 16px;
-line-height: 150%;
-height: 250px;
-vertical-align: middle;
-color: rgba(0, 0, 0, 0.6);
+  &-described {
+    font-family: Open Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 150%;
+    height: 250px;
+    vertical-align: middle;
+    color: rgba(0, 0, 0, 0.6);
+  }
 
-}
-
-&-add { 
-width: 202px;
-height:48px;
-border-radius: 50px;
-background-color: black;
-color: white;
-
-}
-&-closeimage {
-  z-index: 999999;
-  padding: 10px;
-  color: #000;
-  cursor: pointer;
-}
-&-title{
-font-family: Open Sans;
-font-style: normal;
-font-weight: bold;
-font-size: 32px;
-line-height: 44px;
-padding-bottom: 2rem !important;
-}
-&-price{
-  padding-top: 30px;
-font-style: normal;
-font-weight: bold;
-font-size: 26px;
-line-height: 35px;
-color: #000000;
-
-}
-.productPhoto {
-width: 356px;
-height: 507px;
-left: 46px;
-top: 33px;
-}
+  &-add {
+    width: 202px;
+    height: 48px;
+    border-radius: 50px;
+    background-color: black;
+    color: white;
+  }
+  &-closeimage {
+    z-index: 999999;
+    padding: 10px;
+    color: #000;
+    cursor: pointer;
+  }
+  &-title {
+    font-family: Open Sans;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 44px;
+    padding-bottom: 2rem !important;
+  }
+  &-price {
+    padding-top: 30px;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 26px;
+    line-height: 35px;
+    color: #000000;
+  }
+  .productPhoto {
+    width: 356px;
+    height: 507px;
+    left: 46px;
+    top: 33px;
+  }
 }
 </style>
