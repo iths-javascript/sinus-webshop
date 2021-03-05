4a3bb89e9389095c4b10f0a2a9680b423b388131
rutbergphilip <philiprutberg00@gmail.com>
2021-03-05 13:20:28 +0100

    checkout

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -1,114 +1,113 @@
 <template>
-    <div class="container">
-        <div class="top">
-            <img class="top-img" src="../assets/Rectangle 52.svg">
-            <!-- <p class="sinus-store">#SINUS-STORE</p> -->
-        </div>
-         <nav >
-            <p @click="bindCategory('skateboards')">skateboards</p>
-            <p @click="bindCategory('wheels')">wheels</p>
-            <p @click="bindCategory('t-shirts')">t-shirts</p>
-            <p @click="bindCategory('hoodies')">hoodies</p>
-            <p @click="bindCategory('sneakers')">sneakers</p>
-        </nav>
-        <div class="main">
-            <div class="product-container"  v-for="image in imgFile"  
-            v-bind:key="image.index" >
-                <div class="portrait">
-                    <img :src="require(`../assets/${image.imgFile}`)" v-on:click="singleProduct(image._id)" >
-                </div>
-                <p class="title">{{image.title}}</p>
-                <p class="description">{{image.description}}</p>
-                <p class="price">{{image.price}} SEK</p>
-                <button v-on:click="addToCart(image)">add to cart</button>
-            </div>
-        </div>
+  <div class="container">
+    <div class="top">
+      <img class="top-img" src="../assets/Rectangle 52.svg" />
+      <!-- <p class="sinus-store">#SINUS-STORE</p> -->
     </div>
+    <nav>
+      <p @click="bindCategory('skateboards')">skateboards</p>
+      <p @click="bindCategory('wheels')">wheels</p>
+      <p @click="bindCategory('t-shirts')">t-shirts</p>
+      <p @click="bindCategory('hoodies')">hoodies</p>
+      <p @click="bindCategory('sneakers')">sneakers</p>
+    </nav>
+    <div class="main">
+      <div
+        class="product-container"
+        v-for="image in imgFile"
+        v-bind:key="image.index"
+      >
+        <div class="portrait">
+          <img
+            :src="require(`../assets/${image.imgFile}`)"
+            v-on:click="singleProduct(image._id)"
+          />
+        </div>
+        <p class="title">{{ image.title }}</p>
+        <p class="description">{{ image.description }}</p>
+        <p class="price">{{ image.price }} SEK</p>
+        <button v-on:click="addToCart(image)">add to cart</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: "Product",
+  name: "Product",
 
-data() { return {
-    tab: "skateboards"
-}},
+  data() {
+    return {
+      tab: "skateboards",
+    };
+  },
 
-computed: {
+  computed: {
     imgFile() {
-        return this.$store.state.currentProducts;
-    } 
-},
+      return this.$store.state.currentProducts;
+    },
+  },
 
-methods: {
-
+  methods: {
     bindCategory(category) {
-        this.$store.dispatch('loadProducts', category);
+      this.$store.dispatch("loadProducts", category);
     },
 
     singleProduct(id) {
-        this.$router.push(`/single-product/${id}`);
+      this.$router.push(`/single-product/${id}`);
     },
 
     addToCart(item) {
-       this.$store.commit('storeIntoCart', item);
-    }
-},
+      this.$store.commit("storeIntoCart", item);
+    },
+  },
 
-beforeMount() {
-        this.$store.dispatch('drawImage');
-    }
-}
+  beforeMount() {
+    this.$store.dispatch("drawImage");
+  },
+};
 </script>
-<<<<<<< HEAD
-
-=======
->>>>>>> cart
 <style scoped>
-
-@import url('https://fonts.googleapis.com/css2?family=Ropa+Sans&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Ropa+Sans&display=swap");
 
 .container {
-
-    margin:auto;
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    background-color:white;
-     background-image: url('../assets/background-products.svg');
-     background-repeat: no-repeat;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  background-image: url("../assets/background-products.svg");
+  background-repeat: no-repeat;
 }
 
 .top {
-    width:52.5vw;
-    height:70vh;
-    background-color:#F4C3C3;
-    display:flex;
-    align-items:center;
-    justify-content:center;
+  width: 52.5vw;
+  height: 70vh;
+  background-color: #f4c3c3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .main {
-    margin-top:5rem;
-    display:grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-column-gap:30px;
-    grid-template-rows:repeat(2, 1fr);
-    grid-row-gap: 50px;
-    padding-bottom: 100px;
+  margin-top: 5rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 30px;
+  grid-template-rows: repeat(2, 1fr);
+  grid-row-gap: 50px;
+  padding-bottom: 100px;
 }
 
 nav {
-    background-color:white;
-    display:flex;
-    margin-top:3rem;
+  background-color: white;
+  display: flex;
+  margin-top: 3rem;
 }
 
 nav p {
-    margin-left:50px;
-    cursor:pointer;
+  margin-left: 50px;
+  cursor: pointer;
 }
 
 button {
@@ -122,47 +121,43 @@ button {
   border-style: none;
   box-shadow: -6px 6px #2b2b2b;
   margin: 0rem;
-
 }
 
-
-
 .price {
-    font-family: 'Ropa Sans', sans-serif;
-    font-size:24px;
-    color:black;
-    margin-top:10px;
-
+  font-family: "Ropa Sans", sans-serif;
+  font-size: 24px;
+  color: black;
+  margin-top: 10px;
 }
 
 .title {
-    font-family: 'Ropa Sans', sans-serif;
-    font-size:24px;
-    margin-top:15px;
+  font-family: "Ropa Sans", sans-serif;
+  font-size: 24px;
+  margin-top: 15px;
 }
 
 .description {
-    font-family: 'Ropa Sans', sans-serif;
-    font-size:14px;
+  font-family: "Ropa Sans", sans-serif;
+  font-size: 14px;
 }
 
 .portrait {
-   width: 265px;
-   height: 325px;
-   background-color:#CBE9EF;
-   justify-content:center;
-   align-items:center;
-   display:flex;
+  width: 265px;
+  height: 325px;
+  background-color: #cbe9ef;
+  justify-content: center;
+  align-items: center;
+  display: flex;
 }
 
-.product-container p, button {
-    margin-right:90px;
-<<<<<<< HEAD
+.product-container p,
+button {
+  margin-right: 90px;
 }
 
 .top-img {
-    position:relative;
-    box-shadow: -40px 0 0 -20px #B2D1A8;    
+  position: relative;
+  box-shadow: -40px 0 0 -20px #b2d1a8;
 }
 
 /* .sinus-store {
@@ -174,11 +169,4 @@ button {
     letter-spacing: 0.3rem;
     font-size:36px;
 } */
-
-
-
-
 </style>
-=======
-}
->>>>>>> cart
